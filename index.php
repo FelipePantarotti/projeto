75d8713660efdf5c58d11901d0e073226820d7bc
FelipePantarotti <felipepantarotti@gmail.com>
2026-09-14 16:27:39 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
     <title>Document</title>
 </head>
 <body>
+<h1>nome:<?= $nome ?></h1>
+<p>idade:<?= $idade ?></p>
     <?php
     $nome = "Felipe";
     $idade = "16";
@@ -16,7 +18,6 @@
         echo "Você é menor de idade";
     }
     ?>
-    <h1>nome:<?= $nome ?></h1>
-    <p>idade:<?= $idade ?></p>
+
 </body>
 </html>
