5273d1ddb5b2295ea08db57f7e4b3fa6de32f375
FelipePantarotti <felipepantarotti@gmail.com>
2026-09-14 16:19:25 -0300

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,9 @@
 <body>
     <?php
     $nome = "Felipe";
-    echo "<h1>Olá, $nome!</h1>"
+    $idade = "16";
     ?>
+    <h1>nome:<?= $nome ?></h1>
+    <p>idade:<?= $idade ?></p>
 </body>
 </html>
