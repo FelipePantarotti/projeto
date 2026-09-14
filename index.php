daed8f1315be4424e1184f0f5fb8bb79fdf46e6e
FelipePantarotti <felipepantarotti@gmail.com>
2026-09-14 16:56:50 -0300

    estilizando a pagina

--- index.php
+++ index.php
@@ -3,23 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
-    <?php
-    $nome = "Felipe";
-    $idade = "16";
-    $verificar = "";
+    <div class="container">
+        <?php
+        $nome = "Felipe";
+        $idade = "16";
+        $verificar = "";
 
-    if ($idade >= 18){
-        $verificar = "Você é maior de idade";
-    }
-    else {
-        $verificar = "Você é menor de idade";
-    }
-    ?>
-    <h1>nome:<?= $nome ?></h1>
-    <h2>idade:<?= $idade ?></h2>
-    <p><?= $verificar ?></p>
+        if ($idade >= 18){
+            $verificar = "Você é maior de idade";
+        }
+        else {
+            $verificar = "Você é menor de idade";
+        }
+        ?>
+        <h1>nome:<?= $nome ?></h1>
+        <h2>idade:<?= $idade ?></h2>
+        <p><?= $verificar ?></p>
+    </div>
 </body>
 </html>
