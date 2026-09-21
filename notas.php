07f8d377d7d0516b1e6e506ab93aca22ed170cc1
FelipePantarotti <felipepantarotti@gmail.com>
2026-09-21 15:16:35 -0300

    teste da media das notas

--- notas.php
+++ notas.php
@@ -1,5 +1,30 @@
+<?php
+        $nome = "";
+        $idade = 0;
+        $notas = "";
+        $resultado = "";
+
+        if ($_SERVER["REQUEST_METHOD"]=="POST") {
+            $nota1 = $_POST["nota1"];
+            $nota2 = $_POST["nota2"];
+            $nota3 = $_POST["nota3"];
+            $nota4 = $_POST["nota4"];
+            $nota5 = $_POST["nota5"];
+        
+        $media = ($nota1 * 2 + $nota2 * 3 + $nota3 * 1 + $nota4 * 1 + $nota5 * 3) / 10;
+        }
+        if ($media >= 7){
+            $resultado = "aprovado";
+        }
+        else if ($media >= 5){
+            $resultado = "recuperação";
+        }
+        else {
+            $resultado = "reprovado";
+        }
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,5 +41,7 @@
 
         
     </form>
+
+    <h2><?= $nome ?>, idade:<?= $idade ?>, resultado:<?= $resultado ?></h2>
 </body>
 </html>
