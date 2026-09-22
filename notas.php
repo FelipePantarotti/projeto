60af7ec74ae3467bbfb487ed767e3380fa05f939
FelipePantarotti <felipepantarotti@gmail.com>
2026-09-22 14:15:19 -0300

    Update notas.php

--- notas.php
+++ notas.php
@@ -54,7 +54,7 @@
         
     </form>
 
-    <h2><?= $nome ?>, idade:<?= $idade ?>, media calculada:<?= $media ?></h2>
+    <h2><?= $nome ?>, idade:<?= $idade ?>, media calculada:<?= $media ?></h2>,<br><br>
     <h2>situação do aluno: <?= $resultado ?></h2>
 </body>
 </html>
