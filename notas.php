e078cc7814d2b8f7badc3b5a0e11439a85083408
FelipePantarotti <felipepantarotti@gmail.com>
2026-09-22 15:44:24 -0300

    Update notas.php

--- notas.php
+++ notas.php
@@ -19,6 +19,7 @@
             $nota3 = $_POST["nota3"];
             $nota4 = $_POST["nota4"];
             $nota5 = $_POST["nota5"];
+            $erro = $_POST["erro"];
    
         if ($idade == 0 or $idade == ""){
             $erro = "A idade deve ser maior que 0";
@@ -73,17 +74,13 @@
         <button type="submit">Enviar</button>
     </form>
 
-    
-<?php if (empty($erro) && $media !== null): ?>
-    <h2>Aluno: <?= htmlspecialchars($nome) ?></h2>
-    <h2>Idade: <?= $idade ?></h2>
-    <h2>Média calculada: <?= number_format($media, 2, ',', '.') ?></h2>
-    <h2>Situação do aluno: <?= $resultado ?></h2>
+        <h2><?= $erro ?></h2>
+        <h2>Aluno: <?= $nome ?></h2>
+        <h2>Idade:<?= $idade ?></h2>
+        <h2>Média calculada:<?= $media ?></h2>
+        <h2>Situação do aluno: <?= $resultado ?></h2>
+        <h2>Faltam <?= $pontos ?> para atingir a média</h2>
 
-    <?php if ($pontos !== null): ?>
-        <h2>Faltam <?= number_format($pontos, 2, ',', '.') ?> ponto(s) para atingir a média</h2>
-    <?php endif; ?>
-<?php endif; ?>
         <a href="index.php" class="bt-voltar">Voltar ao inicio</a>
 </div>
 </body>
